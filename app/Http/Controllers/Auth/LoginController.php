3e1c0a3a97dc57485e70a6e5b7d061afa4facbbc
Osam-Frimpong Schandorf <osamfrimpong@yahoo.com>
2020-11-24 05:05:24 +0000

    changed login interface

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -41,7 +41,7 @@ class LoginController extends Controller
 
     public function showLoginForm()
     {
-        return view('auth.new_login');
+        return view('auth.login');
     }
 
     protected function authenticated(Request $request, $user)
